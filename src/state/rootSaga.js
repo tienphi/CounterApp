c0661bf216ad952ff5fe8ccffaa26c8cee69e9cc
tienphi <dactienbkba04@gmail.com>
2019-05-29 11:47:22 +0700

    commit

--- src/state/rootSaga.js
+++ src/state/rootSaga.js
@@ -3,8 +3,8 @@ import { all } from 'redux-saga/effects'
 
 export default function* rootSaga() {
     yield all([
-        helloSaga,
-        watchIncrementNumberAsync,
-        watchDecrementNumberAsync
+        helloSaga(),
+        watchIncrementNumberAsync(),
+        watchDecrementNumberAsync()
     ])
 }
